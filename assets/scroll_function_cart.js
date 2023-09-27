81f42f137887d3b07ccc708c27918e6686c8126e
cFelixLuketm <felix.luke.hello@outlook.com>
2023-09-27 16:13:55 +0100

    27th

--- assets/scroll_function_cart.js
+++ assets/scroll_function_cart.js
@@ -13,20 +13,13 @@ const cartHeader = document.querySelector(".cart__title__price__mobile");
 
 if (screenWidthscrollfunctioncart.matches) {
   document.addEventListener("scroll", function (event) {
-    // const productTitle = document.querySelector(".product__title__price__mobile");
-
-
     const scroll = window.pageYOffset
 
      if(scroll > cartWrapperHeight - 850) {
        console.log(cartWrapperHeight)
-       // productTitle.classList.add('active');
-       // addToBag.classList.add('active');
        addToBag.style.bottom = "-6vh";
        cartHeader.style.bottom = "-6vh";
      } else {
-       // productTitle.classList.remove('active');
-       // addToBag.classList.remove('active');
        addToBag.style.bottom = "0vh";
        cartHeader.style.bottom = "6vh";
      }
